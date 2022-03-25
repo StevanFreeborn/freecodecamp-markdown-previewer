7cab64fbeaabd7fbda450b2c345beeb2abf1cc4c
StevanFreeborn <sfreeborn7@gmail.com>
2022-03-25 14:47:10 -0500

    Added dompurify to santize parsed html;

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 class Previewer extends React.Component {
     constructor(props) {
@@ -7,24 +8,30 @@ class Previewer extends React.Component {
     }
 
     render() {
+        // set marked options so that carriage returns can be parsed as line breaks
+        // when rendering the html.
         marked.setOptions({
             breaks: true,
         })
 
         const renderer = new marked.Renderer();
 
+        // override renderer's link function
         renderer.link = function(href, title, text) {
             return `<a target="_blank" href="${href}">${text}</a>`
         }
 
-        const html = marked(this.props.markdown, {renderer: renderer});
+        // sanitize html that was parsed from markdown text
+        const html = DOMPurify.sanitize(
+            marked(this.props.markdown, {renderer: renderer})
+        );
 
         return (
             <div
                 id="previewer"
                 className="previewer"
                 dangerouslySetInnerHTML={{
-                    __html: marked(this.props.markdown, {renderer: renderer})
+                    __html: html
                 }}
             >
             </div>
